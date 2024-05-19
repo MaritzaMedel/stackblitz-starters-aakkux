c1e49be6e2f5805dd4ce8df437a35eaaa3cfef5d
CesarJZB <cesar_zb12@hotmail.com>
2024-05-19 15:28:24 -0600

    Archivos

---
+++ src/app/listado-alumnos/listado-alumnos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-alumnos',
+  standalone: true,
+  imports: [],
+  templateUrl: './listado-alumnos.component.html',
+  styleUrl: './listado-alumnos.component.css'
+})
+export class ListadoAlumnosComponent {
+
+}
